6a06a3736aaa5b4063588d0c8941dd3d5e638863
Seb <sebastien.garcia@cawita.com>
2022-05-20 11:13:03 +0200

    initial commit

---
+++ src/libs/scatter-gl/demo/data/projection.d.ts
@@ -0,0 +1,6 @@
+export interface Data {
+    labels: number[];
+    labelNames: string[];
+    projection: [number, number, number][];
+}
+export declare const data: Data;
---
+++ src/libs/scatter-gl/demo/sequences.d.ts
@@ -0,0 +1,2 @@
+import { Sequence, Point3D, PointMetadata } from '../src/data';
+export declare function makeSequences(points: Point3D[], metadata: PointMetadata[], nSequencesPerLabel?: number, sequenceLength?: number): Sequence[];
